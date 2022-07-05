ca564f7fd3ebd2cb73990b486ef22d5edca88ac9
TudiFrudii <vittoria.ossanna@studenti.unitn.it>
2022-07-05 11:12:44 +0200

    core genes scripts

---
+++ get_core_genes/README.md
@@ -0,0 +1,7 @@
+`get_aa_sequences.sh` and `get_nt_sequences.sh` are script that allow to get core gene sequences of a defined list os SGBs.
+they either retrive amino-acids sequences (aa) or nucleotide sequences (nt).
+They both require to acrivate a conda environment `conda activate chocophlansgb`
+
+These scripts need two arguments:
+- input file with ids
+- output directory
---
+++ get_core_genes/get_nt_sequences.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+input=$1
+output=$2
+
+if [ "$#" -lt 2 ]; then
+    echo "The script needs input file and output directory as params"
+    exit 1
+fi
+
+echo "retriving aa_sequences..."
+
+while read SGB;
+do
+        echo "reading SGB: ${SGB}"
+        python /shares/CIBIO-Storage/CM/scratch/projects/nkarcher_FMT_meta/scripts/uniref90_pangenome/get_core_genes_nt.py --sgb $SGB --output $output -r Jan21
+done < $input
